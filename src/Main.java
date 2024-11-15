a1f4de9994dee2df09639fdefb56e394aa8800a2
Guillermo Arria <guillermo.arriadevoe@gmail.com>
2024-11-15 00:45:26 -0500

    Completed basics tutorial

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Print with a new line statement.");
+        System.out.print("Print with an new line escape sequence.\n");
+        System.out.println("Print with \"other\" escape\tsequences.");
+
+        // Single line comment.
+
+        /*
+        Multi
+        Line
+        Comment.
+         */
+    }
+}
