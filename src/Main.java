9b6a247d81037d5a6c65871baf34e127eef5bc7a
Guillermo Arria <guillermo.arriadevoe@gmail.com>
2024-11-16 16:36:22 -0500

    variables lesson

--- src/Main.java
+++ src/Main.java
@@ -11,5 +11,43 @@ public class Main {
         Line
         Comment.
          */
+
+        /*
+        Data Types in Java
+          >>boolean primitive   t or f
+            byte    primitive   -128 to 127
+            short   primitive   -32k to 32k
+          >>int     primitive   -2 bill to 2 bill
+            long    primitive   -9 quantill to 9 quantill
+
+            float   primitive   fractional up to 6-7 digits, uses f
+          >>double  primitive   fractional up to 15 digits
+
+          >>char    primitive   single char/letter/ASCII, uses ''
+          >>String  reference   char sequence, uses ""
+
+         Primitive vs Reference
+            limited    vs  user defined
+            data       vs  address
+            1 value    vs  can hold multiple values
+            - memory   vs  + memory
+            fast       vs  slower
+         */
+
+        /*
+            int int_var;        // declaration
+            int_var = 123;      // assignment
+        */
+
+        int int_var = 123;  // initialization
+        long lg_var = 1234566532457632575L;
+        byte bt_var = 127;
+        float flt_var = 3.041f;
+        double dbl_var = 123.01;
+        boolean bl_var = false;
+        char chr_var = '@';
+        String str_var = "Aloha";
+
+        System.out.println(str_var + chr_var);
     }
 }
